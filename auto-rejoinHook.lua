4c806d7dc4df6457a7e45b2fc45e8e5c132d1e56
xanyboyo <m.usicboyoalt@gmail.com>
2025-11-19 14:48:16 -0500

    Update auto-rejoinHook.lua

--- auto-rejoinHook.lua
+++ auto-rejoinHook.lua
@@ -1,3 +1,3 @@
-_G.scriptToLoad = _G.args[1]
+_G.scriptToLoad = _G.args
 loadstring(game:HttpGet('https://raw.githubusercontent.com/xanyboyo/private/refs/heads/main/auto-rejoin.lua?token=GHSAT0AAAAAADPMWF5YWW4EGCTAKE7FNDZM2I6DQJA'))()
 print("Loading auto-rejoin from auto-rejoinHook")
