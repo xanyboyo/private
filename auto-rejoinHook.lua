afca356139e6828133bd65b39ad9745fec109976
xanyboyo <m.usicboyoalt@gmail.com>
2025-11-19 15:06:49 -0500

    Delete auto-rejoinHook.lua

--- auto-rejoinHook.lua
+++
@@ -1,6 +0,0 @@
-_G.scriptToLoad = nil
-_G.scriptToLoad = _G.args
-print("Loading auto-rejoin from auto-rejoinHook1")
-loadstring(game:HttpGet('https://raw.githubusercontent.com/xanyboyo/private/refs/heads/main/auto-rejoin.lua'))()
-print("Loading auto-rejoin from auto-rejoinHook2")
-
