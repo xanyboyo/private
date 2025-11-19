8afa2f134b334f7a538b96c6d1fd0755800e72ce
xanyboyo <m.usicboyoalt@gmail.com>
2025-11-19 14:45:12 -0500

    Update auto-rejoin.lua

--- auto-rejoin.lua
+++ auto-rejoin.lua
@@ -1,3 +1,4 @@
+_G.scriptToLoad = _G.scriptToLoad
 local NetworkClient = cloneref(game:GetService("NetworkClient"))
 local TeleportService = cloneref(game:GetService("TeleportService"))
 local Players = cloneref(game:GetService("Players"))
@@ -7,6 +8,13 @@ local args = {
 	[3] = Players.LocalPlayer
 }
 queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/xanyboyo/private/refs/heads/main/auto-rejoin.lua?token=GHSAT0AAAAAADPMWF5YWW4EGCTAKE7FNDZM2I6DQJA'))()")
+-----------------------------------------------------------------------------------------------------------
+if _G.scriptToLoad = 1 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/TheRealAsu/BABFT/refs/heads/main/Loader.lua'))()
+elseif _G.scriptToLoad = 2
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ProBaconHub/ProBaconHubV2/refs/heads/main/LOADER.lua"))()
+end
+-----------------------------------------------------------------------------------------------------------
 NetworkClient.ChildRemoved:Connect(function()
    TeleportService:TeleportToPlaceInstance(unpack(args));
 end)
