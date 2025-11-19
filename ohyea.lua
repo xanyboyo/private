94b04ac1fe5e4be43e4452d8a2e26925a7259f94
xanyboyo <m.usicboyoalt@gmail.com>
2025-11-18 19:19:25 -0500

    Update ohyea.lua

--- ohyea.lua
+++ ohyea.lua
@@ -1,2 +1,3 @@
 setfpscap(30)loadstring(game:HttpGet("https://www.highlighthub.vip/api/get-loader?id=cv0k2d"))()
+if not game:IsLoaded() then game.Loaded:Wait() task.wait(3) end
 queue_on_teleport("loadstring(\"https://raw.githubusercontent.com/xanyboyo/private/refs/heads/main/ohyea.lua?token=GHSAT0AAAAAADPMWF5YW4COKVZOUX37RLUY2I5BLXQ\")")
